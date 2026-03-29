d4701c31224e9de9a6fdf5e304f12b2ec7410ea2
erik ny <erik.nyy@gmail.com>
2026-03-29 19:59:52 +0200

    changes again

--- weatherApi.js
+++ weatherApi.js
@@ -70,10 +70,9 @@ async function fetchWeatherData(lat, lon) {
 }
 
 function getWeatherImage(code) {
-    if (code === 0) return "images/Soligt.png";
-    if (code <= 3) return "images/Molningt.png";
-    if (code <= 67) return "images/Regn.png";
+    if (code === 0) return "images/soligt.png";
+    if (code <= 3) return "images/molningt.png";
+    if (code <= 67) return "images/regn.png";
     return "images/halvklart-regn.png";
 }
-
 getLocation();
